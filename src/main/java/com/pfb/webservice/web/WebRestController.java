31ddc9b42591fa3cb6f900da2984aa27d299d754
DESKTOP-5533OO2\jhleon <jhleon869@gmail.com>
2018-05-10 21:51:16 +0900

    no message

--- src/main/java/com/pfb/webservice/web/WebRestController.java
+++ src/main/java/com/pfb/webservice/web/WebRestController.java
@@ -34,6 +34,7 @@ public class WebRestController {
     @GetMapping("/profile")
     public String getProfile () {
         return Arrays.stream(env.getActiveProfiles())
+        		.skip(1)
                 .findFirst()
                 .orElse("");
     }
